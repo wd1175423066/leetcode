9c7a78b286e59593659649025ba599c5d86c6c5d
diwu <diwu1294@hust.edu.cn>
2023-03-02 22:19:04 +0800

    20230302

---
+++ 416.分割等和子集.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=416 lang=cpp
+ *
+ * [416] 分割等和子集
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        vector<int> dp(10001, 0);
+        int sum = 0;
+        for(const int num : nums) {
+            sum += num;
+        }
+        if(sum % 2 == 1) return false;
+        int target = sum / 2;
+        // dp
+        for(int i = 0; i < nums.size(); ++i) {
+            for(int j = target; j >= nums[i]; --j) {
+                dp[j] = max(dp[j], dp[j - nums[i]] + nums[i]);
+            }
+        }
+        if(dp[target] == target) return true;
+        return false;
+    }
+};
+// @lc code=end
+
